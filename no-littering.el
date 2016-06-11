196755d946559abf4876427d1e3a1c77a42f0f3f
Chunyang Xu <xuchunyang.me@gmail.com>
2016-06-11 19:04:59 +0800

    emms: theme emms-directory

--- no-littering.el
+++ no-littering.el
@@ -168,6 +168,7 @@ This variable has to be set before `no-littering' is loaded.")
 ;;; Third-party packages
 
     (setq anaconda-mode-installation-directory (etc "anaconda-mode/"))
+    (setq emms-directory                   (var "emms/"))
     (setq helm-adaptive-history-file       (var "helm-adaptive-history.el"))
     (setq helm-github-stars-cache-file     (var "helm-github-stars-cache"))
     (setq mc/list-file                     (var "mc-list.el"))
