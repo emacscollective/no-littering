7f9e6f87ba02f44a82b6c88f485227341c48602a
Diego A. Mundo <therockmandolinist@users.noreply.github.com>
2016-11-27 11:09:37 -0500

    emojify: theme emojify-emojis-dir

--- no-littering.el
+++ no-littering.el
@@ -225,6 +225,7 @@ This variable has to be set before `no-littering' is loaded.")
       `(make-directory ,(var "company/") t))
     (setq company-statistics-file          (var "company/statistics.el"))
     (setq emms-directory                   (var "emms/"))
+    (setq emojify-emojis-dir               (var "emojify/"))
     (eval-after-load 'helm
       `(make-directory ,(var "helm/") t))
     (setq helm-adaptive-history-file       (var "helm/adaptive-history.el"))
