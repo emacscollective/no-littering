9f2db53a6dc78c9e2f618b211e26a1b78dd602e1
therockmandolinist <diegoamundo@gmail.com>
2016-11-27 13:40:25 -0500

    xkcd: theme xkcd-cache-dir

--- no-littering.el
+++ no-littering.el
@@ -245,6 +245,7 @@ This variable has to be set before `no-littering' is loaded.")
     (setq sx-cache-directory               (var "sx-cache/"))
     (setq undo-tree-history-directory-alist (list (cons "." (var "undo-tree-hist/"))))
     (setq user-emacs-ensime-directory      (var "ensime/"))
+    (setq xkcd-cache-dir                   (var "xkcd/"))
     (eval-after-load 'yasnippet
       `(make-directory ,(etc "yas-snippets/") t))
     (setq yas-snippet-dirs                 (list (etc "yas-snippets/") 'yas-installed-snippets-dir))
