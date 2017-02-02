0227c5eea5b9f3fb056a7ef6052ef6f076371bde
Radon Rosborough <radon.neon@gmail.com>
2017-02-01 17:56:26 -0800

    historian: theme `historian-save-file'

--- no-littering.el
+++ no-littering.el
@@ -250,6 +250,7 @@ directories."
       `(make-directory ,(var "helm/") t))
     (setq helm-adaptive-history-file       (var "helm/adaptive-history.el"))
     (setq helm-github-stars-cache-file     (var "helm/github-stars-cache.el"))
+    (setq historian-save-file              (var "historian-save.el"))
     (setq irony-user-dir                   (var "irony/"))
     (setq jabber-avatar-cache-directory    (var "jabber/avatar-cache"))
     (eval-after-load 'jabber
