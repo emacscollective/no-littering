82b19f28e0c5cbeac4d2c22472f3b95a7a0690a1
Jonas Bernoulli <jonas@bernoul.li>
2018-01-08 12:04:48 +0100

    Bump copyright years

--- no-littering.el
+++ no-littering.el
@@ -1,6 +1,6 @@
 ;;; no-littering.el --- help keeping ~/.emacs.d clean
 
-;; Copyright (C) 2016-2017  Jonas Bernoulli
+;; Copyright (C) 2016-2018  Jonas Bernoulli
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/emacscollective/no-littering
