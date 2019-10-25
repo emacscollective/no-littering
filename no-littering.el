effed6caa63423b366a072fe31e5494389f9281e
cage <cage-invalid@invalid>
2019-10-25 18:42:30 +0200

    annotate.el: theme annotate-file
    This is the only data file of the package and stores s-expressions.

--- no-littering.el
+++ no-littering.el
@@ -254,6 +254,7 @@ This variable has to be set before `no-littering' is loaded.")
     (setq ac-comphist-file                 (var "ac-comphist.el"))
     (setq amx-save-file                    (var "amx-save.el"))
     (setq anaconda-mode-installation-directory (var "anaconda-mode/"))
+    (setq annotate-file                    (var "annotations.el"))
     (setq async-byte-compile-log-file      (var "async-bytecomp.log"))
     (eval-after-load 'bbdb
       `(make-directory ,(var "bbdb/") t))
