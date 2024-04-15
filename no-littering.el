554d890c5d5b59ebcf83c67256d0012e23cd07f0
Jonas Bernoulli <jonas@bernoul.li>
2024-04-15 17:45:36 +0200

    Update contact information

--- no-littering.el
+++ no-littering.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2016-2024 Jonas Bernoulli
 
-;; Author: Jonas Bernoulli <jonas@bernoul.li>
+;; Author: Jonas Bernoulli <emacs.no-littering@jonas.bernoulli.dev>
 ;; Homepage: https://github.com/emacscollective/no-littering
 ;; Keywords: convenience
 
