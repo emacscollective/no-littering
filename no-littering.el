7de4e0a88a35e8c56d433fba27512bef38975f24
Jonas Bernoulli <jonas@bernoul.li>
2025-06-01 00:21:47 +0200

    Release version 1.7.7

--- no-littering.el
+++ no-littering.el
@@ -6,8 +6,8 @@
 ;; Homepage: https://github.com/emacscollective/no-littering
 ;; Keywords: convenience
 
-;; Package-Version: 1.7.6
-;; Package-Requires: ((emacs "26.1") (compat "30.0.1.0"))
+;; Package-Version: 1.7.7
+;; Package-Requires: ((emacs "26.1") (compat "30.1"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
