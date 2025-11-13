cf9d195b6dab3276ba1431eafedee2eeae6b4313
Jonas Bernoulli <jonas@bernoul.li>
2025-11-13 18:36:19 +0100

    Publish documentation using webdav
    Re magit/magit#5472.

--- default.mk
+++ default.mk
@@ -1,14 +1,13 @@
 TOP := $(dir $(lastword $(MAKEFILE_LIST)))
 
+DOMAIN ?= emacsmirror.org
+
 PKG = no-littering
 
 ELS   = $(PKG).el
 ELCS  = $(ELS:.el=.elc)
 
 DEPS  = compat
-
-DOMAIN      ?= emacsmirror.net
-CFRONT_DIST ?= E1IXJGPIOM4EUW
 
 EMACS      ?= emacs
 EMACS_ARGS ?=
@@ -17,3 +16,6 @@ LOAD_PATH  ?= $(addprefix -L ../,$(DEPS))
 LOAD_PATH  += -L .
 
 ORG_LOAD_PATH ?= -L ../../htmlize
+
+RCLONE      ?= rclone
+RCLONE_ARGS ?= -v
