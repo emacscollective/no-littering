446ca2e41f3f757546dbf5777e14f334bbff515c
Jonas Bernoulli <jonas@bernoul.li>
2025-11-30 19:43:44 +0100

    make: Add new EMACS_Q_ARG variable
    It defaults to "-Q" but users can instead use "-q", which is useful if
    "site-start.el" contains essential settings.  Also add `EMACS_BATCH'
    and `EMACS_ORG', and rework use of related variables.

--- default.mk
+++ default.mk
@@ -9,13 +9,15 @@ ELCS  = $(ELS:.el=.elc)
 
 DEPS  = compat
 
-EMACS      ?= emacs
-EMACS_ARGS ?=
+LOAD_PATH     ?= $(addprefix -L ../../,$(DEPS))
+LOAD_PATH     += -L .
+ORG_LOAD_PATH ?= -L ../../org/lisp
 
-LOAD_PATH  ?= $(addprefix -L ../,$(DEPS))
-LOAD_PATH  += -L .
-
-ORG_LOAD_PATH ?= -L ../../htmlize
+EMACS       ?= emacs
+EMACS_ARGS  ?=
+EMACS_Q_ARG ?= -Q
+EMACS_BATCH ?= $(EMACS) $(EMACS_Q_ARG) --batch $(EMACS_ARGS) $(LOAD_PATH)
+EMACS_ORG   ?= $(EMACS) $(EMACS_Q_ARG) --batch $(EMACS_ARGS) $(ORG_LOAD_PATH)
 
 RCLONE      ?= rclone
 RCLONE_ARGS ?= -v
