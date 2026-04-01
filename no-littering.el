db60be0939f31eae0cfc537918503a13b028fa56
Jonas Bernoulli <jonas@bernoul.li>
2026-04-01 14:23:19 +0200

    Release version 1.8.6

--- no-littering.el
+++ no-littering.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/emacscollective/no-littering
 ;; Keywords: convenience
 
-;; Package-Version: 1.8.5
+;; Package-Version: 1.8.6
 ;; Package-Requires: (
 ;;     (emacs  "26.1")
 ;;     (compat "30.1"))
